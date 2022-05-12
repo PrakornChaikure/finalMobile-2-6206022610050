0af080db57127292e8a838e48fbb3a84bc3bb45e
PrakornChaikure <6206022610050@fitm.kmutnb.ac.th>
2022-05-12 12:19:28 +0700

    asd

---
+++ my_project/lib/database.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class Database {
+  FirebaseFirestore firestore;
+  initiliase() {
+    firestore = FirebaseFirestore.instance;
+  }
+
+  Future<void> create(String name, String weight, String height, String sex,
+      String year) async {
+    try {
+      await firestore.collection("BMI").add({
+        'height': height,
+        'name': name,
+        'weight': weight,
+        'sex': sex,
+        'year': year,
+        'times': FieldValue.serverTimestamp()
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> delete(String id) async {
+    try {
+      await firestore.collection("BMI").doc(id).delete();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<List> read() async {
+    QuerySnapshot querySnapshot;
+    List docs = [];
+    try {
+      querySnapshot = await firestore.collection('BMI').orderBy('times').get();
+      if (querySnapshot.docs.isNotEmpty) {
+        for (var doc in querySnapshot.docs.toList()) {
+          Map a = {
+            "id": doc.id,
+            "name": doc['name'],
+            "height": doc["height"],
+            "weight": doc["weight"],
+            "sex": doc["sex"],
+            "year": doc["year"],
+          };
+          docs.add(a);
+        }
+        return docs;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> update(String id, String name, String weight, String height,
+      String sex, String year) async {
+    try {
+      await firestore.collection("BMI").doc(id).update({
+        'name': name,
+        'weight': weight,
+        'height': height,
+        'sex': sex,
+        'year': year,
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
